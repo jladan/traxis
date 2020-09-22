a96749d82624164d5836a5ee7ee33a3a477e403a
John Ladan <jladan@physics.utoronto.ca>
2020-09-22 12:38:46 -0400

    fix a couple of typos in setup fields

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 import traxis
 
 setup(
-    name='Traxis',
+    name='traxis',
     version=traxis.__version__,
     url='https://gitlab.physics.utoronto.ca/advanced-lab/traxis',
     author='Syed Haider Abidi, Nooruddin Ahmed, Christopher Dydul',
@@ -18,6 +18,6 @@ setup(
     license='GPL3',
     description='An application to measure bubble-chambre images for the UofT advanced physics lab.',
     long_description=open('README.md').read(),
-    long_description_content_type='text/markdon',
+    long_description_content_type='text/markdown',
     scripts=['runtraxis']
 )
